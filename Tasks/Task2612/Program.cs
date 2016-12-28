21bade831127dcbaf13ef09b583c6ea6133392b1
VitalyKiktenko <vitaly_kiktenko@mail.ru>
2016-12-28 21:27:40 +0200

    Add files via upload

---
+++ Tasks/Task2612/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainBooks();
+        }
+
+        //Task1
+        static void MainBooks()
+        {
+            IEnumerable<Book> books = new List<Book>()
+            {
+                new Book { Name = "FirstBookName", Year = 1980 },
+                new Book { Name = "LINQBookName", Year = 1904 },
+                new Book { Name = "ThirdBookName", Year = 2000 }
+            };
+            var result = books.Where(book => book.Name.Contains("LINQ") && DateTime.IsLeapYear(book.Year));
+        }
+
+        //Task2
+        static void Words()
+        {
+            string[] arrayWords = { "Добро", "Пожаловать" };
+            var result = arrayWords.SelectMany(word => word).Distinct().Count();
+        }
+
+        //Task3
+        static void DoubleFigures()
+        {
+            int[] arrayFigures = { 14, 12, 23, 20, 33, 32 };
+            arrayFigures.OrderBy(x => x);
+        }
+    }
+
+    class Book
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
